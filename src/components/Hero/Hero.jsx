e1151536bae9e8ac80c26484ceef66cf2e52e0a0
ajaykumardev32 <ajayk@dlt.sg>
2025-06-07 17:08:09 +0530

    Added java skills

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ export const Hero = () => {
         <h1 className={styles.title}>Hi, I'm Ajay </h1>
         <p className={styles.description}>
           I'm a full-stack developer with 2+ years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          Java. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:aajaykumarr32@gmail.com" className={styles.contactBtn}>
           Contact Me
